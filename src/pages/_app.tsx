e85c46dd149fd1cbac1513d12a5f81b5517a2ef4
wramadhan <superwahyoe@gmail.com>
2023-07-06 06:33:07 +0700

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
+import GA from '@/components/GA/GA'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ReactElement } from 'react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps): ReactElement {
+  return (
+    <>
+      <GA measurementId='G-X8LQ9WW8ZK' />
+      <Component {...pageProps} />
+    </>
+  )
 }
